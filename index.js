2aa670557043cde6c4394e074f2e35c49eeb7a42
srsowmiya <sowmiyasundaram1040@gmail.com>
2025-10-15 20:51:54 +0530

    server tested and verified

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express=require('express')
 const app= express()
 const port = 8001
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port,()=>{
     console.log('server started running on the port') }
 )
