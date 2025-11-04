bd373e57ec2e3b0eda3add4697f5fd767accd9b3
srsowmiya <sowmiyasundaram1040@gmail.com>
2025-11-04 15:16:02 +0530

    Fixed the issues for vercel deployment

--- controllers/url.js
+++ controllers/url.js
@@ -1,21 +1,17 @@
-const {nanoid}= require("nanoid")
+const { nanoid } = require("nanoid");
+const URL = require("../models/url");
 
+async function generateShortUrl(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
 
-const URL=require("../models/url")
+  const shortId = nanoid(8);
+  await URL.create({
+    short_id: shortId,
+    redirect_url: body.url,
+  });
 
-async function generateShortUrl(req,res)
-{
-    const body= req.body
-    if(!body.url) return res.status(400).json({error:'url is required'})
-    const shortId= nanoid(8)
-    await URL.create(
-        {
-            short_id:shortId,
-            redirect_url:body.url
-        }
-    )
-
-    return res.json({id:shortId})
+  return res.json({ id: shortId });
 }
 
-module.exports={generateShortUrl}
+module.exports = { generateShortUrl };
